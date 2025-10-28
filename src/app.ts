2d07c034f1ead82285dc720625fed7f8cd6a434b
AkashP4769 <AwesomeAkash47@gmail.com>
2025-10-28 13:26:37 +0530

    reverting back a few things

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 // src/app.ts
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import serverless from '@hono/node-server'; // or 'serverless-http'
 import userRoutes from './routes/userRoutes';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
